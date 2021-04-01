0e947a44a5daf7cc1fcf7903f7674aefc4b17f2c
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-01 11:07:40 +0900

    エラーハンドリング追記

--- src/Components/App.js
+++ src/Components/App.js
@@ -19,6 +19,15 @@ class App extends Component{
     };
   }
 
+  // 重複する記述をまとめる(エラー文)
+  setErrorMessage(message){
+    this.setState({
+      address: message,
+      lat: '-',
+      lng: '-',
+    });
+  }
+
   // 関数作成
   handlePlaceSubmit(place){
     // 第一引数にAPIURL変数、第二引数にaddress
@@ -27,17 +36,35 @@ class App extends Component{
     .get(GEOCODE_ENDPOINT, { params: {address: place, key: MAP_API_KEY } })
     // thenの引数に結果が返ってくる
     .then((results) => {
+      const status = results.data.status; // 結果判定用変数
       console.log(results.data);
-      // resultsに入ってきているdataのresultsの0番目を格納
-      const data = results.data.results[0];
-      // 上記のdataのgeometry>locationを格納
-      const location = data.geometry.location;
-      this.setState({
-        // dataのなかのアドレス部分を格納
-        address: data.formatted_address,
-        lat: location.lat,
-        lng: location.lng,
-      });
+      console.log(status);
+
+      // 結果をもとに出力を決める
+      switch (status){
+        case 'OK': {  //OKが返ってきている場合
+          const data = results.data.results[0]; // resultsに入ってきているdataのresultsの0番目を格納
+          const location = data.geometry.location;  // 上記のdataのgeometry>locationを格納
+          this.setState({
+            // dataのなかのアドレス部分を格納
+            address: data.formatted_address,
+            lat: location.lat,
+            lng: location.lng,
+          });
+          break;
+        }
+        case 'ZERO_RESULTS': {
+          this.setErrorMessage("結果が見つかりません");
+          break;
+        }
+        default: {
+          this.setErrorMessage("エラーが発生しました");
+        }
+      }
+    })
+    // try-catch
+    .catch((error) => {
+      this.setErrorMessage("エラーが発生しました")
     });
 
   }
@@ -51,7 +78,7 @@ class App extends Component{
       {/* importしたcomponentを出力 onClickイベントを設置 */}
       <SearchForm onSubmit={place => this.handlePlaceSubmit(place) } />
 
-      {/*  */}
+      {/* 結果出力 */}
       <Result
         address={this.state.address}
         lat={this.state.lat}
